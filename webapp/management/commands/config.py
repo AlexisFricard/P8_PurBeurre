91fd3c4e1983e443f9519bc1ee488b56edad4b6b
Fricard Alexis <a.fricardpro@gmail.com>
2021-05-20 14:50:38 +0200

    del old connexion

--- webapp/management/commands/config.py
+++ webapp/management/commands/config.py
@@ -1,21 +1,3 @@
-""" CONFIG TO CONNECT DB """
-
-import psycopg2
-import os
-
-""" POSTGRE SETTINGS """
-settings = os.environ.get("Psql_purbeurre").split()
-
-conn = psycopg2.connect(
-    user=settings[0],
-    password=settings[1],
-    host=settings[2],
-    port="",
-    database=settings[3]
-)
-
-cur = conn.cursor()
-
 """ CONSTANTS SETTING """
 nb_categories = 50
 nb_of_products = 500
