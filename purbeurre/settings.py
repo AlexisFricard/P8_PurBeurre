f05ee286240d117d3f4ee4fe522b49421b2b2bdc
Fricard Alexis <a.fricardpro@gmail.com>
2021-05-25 10:54:56 +0200

    fix bug

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -8,15 +8,15 @@ import dj_database_url
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'purbeurre',
-        'USER': 'devpurbeurre',
-        'PASSWORD': 'passwordpb',
-        'HOST': 'localhost',
-        'PORT': '',
-    },
-}
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'purbeurre',
+            'USER': 'devpurbeurre',
+            'PASSWORD': 'passwordpb',
+            'HOST': 'localhost',
+            'PORT': '',
+        },
+    }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'purbeurre.wsgi.application'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
@@ -100,8 +100,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
@@ -121,9 +119,11 @@ if os.environ.get('ENV') == 'PRODUCTION':
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
     STATICFILES_STORAGE = (
-        "whitenoise.storage.CompressedManifestStaticFilesStorage"  # noqa
+        'whitenoise.storage.CompressedManifestStaticFilesStorage'
     )
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'purbeurre/static'),)
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'purbeurre/static'),
+    )
 
 else:
     DEBUG = True
